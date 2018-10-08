0b9f5f94417381660a9db0e9558f875a3b996331
Cedric David <cedric.david@btopenworld.com>
2018-10-08 06:48:49 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-// Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div className="movie-card">
+        <img
+          src={this.props.poster}
+          alt={this.props.title}
+        />
+        <h2>{this.props.title}</h2>
+        <small>Genres: {this.props.genres.join(', ')}</small>
+      </div>
+    );
+  }
+}
+const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+
+ReactDOM.render(
+  <MovieCard
+    title="Jurassic World"
+    poster="http://image.tmdb.org/t/p/w342/jjBgi2r5cRt36xF6iNUEhzscEcb.jpg"
+    genres={jurassicWorldGenres}
+  />,
+  document.getElementById('root')
+);
