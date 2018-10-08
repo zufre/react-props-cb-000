5d1d89773ca67b5f4ed5f070bc72bdc51747f724
Cedric David <cedric.david@btopenworld.com>
2018-10-08 06:45:37 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div className="movie-card">
+        <img  
+          src={this.props.poster} 
+          alt={this.props.title} 
+        />
+        <h2>{this.props.title}</h2>
+        <small>Genres: {this.props.genres.join(', ')}</small>
+      </div>
+    );
+  }
+}
