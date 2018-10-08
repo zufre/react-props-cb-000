f8ccef40dceb4bcfd8e240e9294a2927725e461c
Cedric David <cedric.david@btopenworld.com>
2018-10-08 06:46:07 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ class MovieCard extends React.Component {
   render() {
     return (
       <div className="movie-card">
-        <img  
-          src={this.props.poster} 
-          alt={this.props.title} 
+        <img
+          src={this.props.poster}
+          alt={this.props.title}
         />
         <h2>{this.props.title}</h2>
         <small>Genres: {this.props.genres.join(', ')}</small>
@@ -16,3 +16,13 @@ class MovieCard extends React.Component {
     );
   }
 }
+const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+ 
+ReactDOM.render(
+  <MovieCard
+    title="Jurassic World"
+    poster="http://image.tmdb.org/t/p/w342/jjBgi2r5cRt36xF6iNUEhzscEcb.jpg"
+    genres={jurassicWorldGenres} 
+  />,
+  document.getElementById('root')
+);
